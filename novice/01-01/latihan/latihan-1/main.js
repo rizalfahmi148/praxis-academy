d05fa22fb3ab69f8cc03f100ab0e787caa9aed47
rizalfahmi <ahmrizalfahmi@gmail.com>
2020-10-06 16:00:54 +0700

    perkenalan

---
+++ novice/01-01/latihan/latihan-1/main.js
@@ -0,0 +1,11 @@
+let a="Ahmad Rizal Fahmi";
+console.log("Nama Saya: " +a);
+document.write("Nama Saya: " + a);
+
+let b="Bandung Baru, Adiluwih, Pringsewu, Lampung";
+console.log("Alamat Saya: " +b);
+document.write("Alamat Saya: " + b);
+
+let c="Ingin mempeljari frontend ";
+console.log("Tujuan Saya: " +c);
+document.write("Tujuan Saya: " + c);
