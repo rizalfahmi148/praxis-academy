8b1217e042a51378d7ba87f89cad811f7b33834d
rizalfahmi <ahmrizalfahmi@gmail.com>
2020-10-07 16:52:14 +0700

    latihan2

---
+++ novice/01-02/latihan/main2.js
@@ -0,0 +1,15 @@
+let x = prompt('masuk', '0');
+let y = prompt('masukan no', '0');
+
+
+if (x > y ) {
+    console.log('masukan no' + 9);
+} else if (x < y ) {
+    console.log('masukan no' + 3);
+} else if (x = y) {
+    console.log('masukan no' + 6)
+}
+ else {
+     console.log('masukan no');
+}
+
