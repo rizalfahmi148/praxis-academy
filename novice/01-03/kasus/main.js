16195dd4d2f8ef5121098cfe5b7b18bfbe2840f7
hdinjos <hdinjos@gmail.com>
2020-10-12 17:01:22 +0700

    12-10-2020

---
+++ novice/01-03/kasus/main.js
@@ -0,0 +1,9 @@
+function semangat(jobTitle, geoLocation, partner, numKids) {
+    var future = 'must keep the spirit ' + jobTitle + ' give up ' + geoLocation + ' married ' +
+   partner + ' ' + '' + numKids + '.';
+    console.log(future);
+}
+
+semangat('ojo sak geleme dewe', 'pikirke lek arep ngomong', 'paham', 3);
+semangat('Must keep the spirit', 'never give up', 'i love you', 3000);
+semangat('jangan pernah menyerah', 'fokus', 'mantaaap',1);
---
+++ novice/01-03/kasus/main2.js
@@ -0,0 +1,8 @@
+function calculateDogAge(age) {
+    var dogYears = 7*age;
+    console.log(dogYears + " years old in dog years!");
+}
+
+calculateDogAge(10023);
+calculateDogAge(0.5);
+calculateDogAge(12);
